42c07a680037464b8bd86777418ad6a38b2a33d0
Haoyu_1231 <142586802+Haoyu-1231@users.noreply.github.com>
2025-03-09 18:07:44 +0800

    Add files via upload

---
+++ WeOS/sysinfo/sysinfo.c
@@ -0,0 +1,19 @@
+#include "apilib.h"
+
+void HariMain(void)
+{
+	int win;
+	char buf[350 * 200];
+	win = api_openwin(buf, 350, 200, -1, "系统信息");
+	api_putstrwin(win, 20, 38, 0, 12, "RedSoil WeOS");
+	api_putstrwin(win, 20, 58, 0, 21, "1.0 Beta 1 (Build 03)");
+	api_putstrwin(win, 20, 78, 1, 18, "此版本为测试版本！");
+	api_putstrwin(win, 21, 78, 1, 18, "此版本为测试版本！");
+	api_putstrwin(win, 20, 128, 0, 36, "版权所有 (C) 2012-2025 RedSoil 公司.");
+	for (;;) {
+		if (api_getkey(1) == 0x0a) {
+			break;
+		}
+	}
+	api_end();
+}
